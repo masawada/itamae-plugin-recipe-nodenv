f33aa0f27544cb879242689037e1e531152dd9a0
Masayoshi Wada <developer@andantesoftware.com>
2019-01-03 00:39:56 +0900

    remove unnecessary files

--- spec/itamae/plugin/recipe/nodenv_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Itamae::Plugin::Recipe::Nodenv do
-  it "has a version number" do
-    expect(Itamae::Plugin::Recipe::Nodenv::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
