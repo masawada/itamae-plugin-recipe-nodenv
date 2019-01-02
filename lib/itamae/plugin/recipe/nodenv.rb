7756a74cd0dd195c819c264b1cf7e46cf51ae83f
Masayoshi Wada <developer@andantesoftware.com>
2019-01-03 00:39:40 +0900

    first commit

---
+++ lib/itamae/plugin/recipe/nodenv.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/nodenv/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Nodenv
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/itamae/plugin/recipe/nodenv_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Itamae::Plugin::Recipe::Nodenv do
+  it "has a version number" do
+    expect(Itamae::Plugin::Recipe::Nodenv::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
