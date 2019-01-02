e7a675cc2b7bc61e8f1c8b03a738c37a650eb5f1
Masayoshi Wada <developer@andantesoftware.com>
2019-01-03 00:46:27 +0900

    add install.rb

---
+++ lib/itamae/plugin/recipe/nodenv/install.rb
@@ -0,0 +1,40 @@
+scheme      = node[:nodenv][:scheme]
+nodenv_root = node[:nodenv][:nodenv_root]
+
+git nodenv_root do
+  repository "#{scheme}://github.com/nodenv/nodenv.git"
+  user node[:nodenv][:user] if node[:nodenv][:user]
+end
+
+directory File.join(nodenv_root, 'plugins') do
+  user node[:nodenv][:user] if node[:nodenv][:user]
+end
+
+git "#{nodenv_root}/plugins/node-build" do
+  repository "#{scheme}://github.com/nodenv/node-build.git"
+  user node[:nodenv][:user] if node[:nodenv][:user]
+end
+
+nodenv_init = <<-EOS
+  export NODENV_ROOT=#{nodenv_root}
+  export PATH="#{nodenv_root}/bin:${PATH}"
+  eval "$(nodenv init -)"
+EOS
+
+node[:nodenv][:versions].each do |version|
+  execute "nodenv install #{version}" do
+    command "#{nodenv_init} nodenv install #{version}"
+    not_if  "#{nodenv_init} nodenv versions | grep #{version}"
+    user node[:nodenv][:user] if node[:nodenv][:user]
+  end
+end
+
+if node[:nodenv][:global]
+  node[:nodenv][:global].tap do |version|
+    execute "nodenv global #{version}" do
+      command "#{nodenv_init} nodenv global #{version}"
+      not_if  "#{nodenv_init} nodenv version | grep #{version}"
+      user node[:nodenv][:user] if node[:nodenv][:user]
+    end
+  end
+end
